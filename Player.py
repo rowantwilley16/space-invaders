e64997a7dee30365ff3efe30d5e06b9f87cdba14
Rowan Twilley <twilley.rowan@gmail.com>
2024-04-05 10:14:52 +0200

    updated the player model

--- Player.py
+++ Player.py
@@ -6,6 +6,9 @@ from Projectile import Projectile
 width = 50
 height = 50  
 
+# Set the image path for the player
+player_image_path = r"assets/player.png"
+
 class Player:
     def __init__(self, x, y):
         self.x = x
@@ -13,8 +16,15 @@ class Player:
         self.width = width
         self.height = height
 
+        self.image = pygame.image.load(player_image_path)
+
+        # Scale the player image to fit the player dimensions
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+
+
     def draw(self, window):
-        pygame.draw.rect(window, (0, 255, 0), (self.x, self.y, self.width, self.height))
+        #pygame.draw.rect(window, (0, 255, 0), (self.x, self.y, self.width, self.height))
+        window.blit(self.image, (self.x, self.y))
 
     def move(self, direction):
         if direction == "left" and self.x > 0:
